c4c35da10f138d5dd2f7330cd0e9c8dc2e7f8549
Benoit-Pouliot <pouliot.benoit@gmail.com>
2017-12-02 12:04:33 -0500

    setup paths

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 
 import sys
-import os
-
-myPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'LDEngine')
-if myPath not in sys.path:
-    sys.path.insert(0, myPath)
 
 import LDEngine.ldLib
 from LDEngine.ldLib.Sprites.GenericEnemy import GenericEnemy

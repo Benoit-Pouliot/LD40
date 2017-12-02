8f6edaa30de3f6e63b2cf7c7d67981ecf75be402
Benoit-Pouliot <pouliot.benoit@gmail.com>
2017-12-02 11:19:35 -0500

    simple sample to add the path of LDEngine
    Temporary solution to use LDEngine

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
 import sys
+import os
+
+myPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'LDEngine')
+if myPath not in sys.path:
+    sys.path.insert(0, myPath)
+
+import LDEngine.ldLib
+from LDEngine.ldLib.Sprites.GenericEnemy import GenericEnemy
 
 if __name__ == '__main__':
 
